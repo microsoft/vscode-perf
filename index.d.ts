d3ef51213ecc0103e6f9ccb8fce4640493a7199c
Sandeep Somavarapu <sasomava@microsoft.com>
2023-01-17 18:06:50 +0100

    fix typings

--- index.d.ts
+++ index.d.ts
@@ -40,4 +40,4 @@ export interface Options {
 	readonly profAppendTimers?: string;
 }
 
-export function run(options: Options[]): Promise<void>;
+export function run(options: Options): Promise<void>;
